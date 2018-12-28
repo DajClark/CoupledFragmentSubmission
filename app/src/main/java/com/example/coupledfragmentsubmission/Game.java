fcbb937b233146bfe54986b65118a017599f8c0b
Daniel Clark <dajclark@googlemail.com>
2018-12-28 21:50:59 +0000

    finished creating model class

---
+++ app/src/main/java/com/example/coupledfragmentsubmission/Game.java
@@ -0,0 +1,67 @@
+package com.example.coupledfragmentsubmission;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Game {
+
+    private UUID gameID;
+    private String title;
+    private String platform;
+    private String description;
+    private Date dateComplete;
+    private boolean isComplete;
+
+    public Game() {
+        this.gameID = UUID.randomUUID();
+        this.dateComplete = new Date();
+    }
+
+    public UUID getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(UUID gameID) {
+        this.gameID = gameID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateComplete() {
+        return dateComplete;
+    }
+
+    public void setDateComplete(Date dateComplete) {
+        this.dateComplete = dateComplete;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+}
